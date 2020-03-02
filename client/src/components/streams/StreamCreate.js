25cfbbce3c9fc2f3f08afb1f976521612b14566b
Roman Onishchenko <ro@rollncode.com>
2020-03-02 18:14:51 +0200

    api json-server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ touched, error }) {
@@ -25,6 +27,7 @@ class StreamCreate extends React.Component {
 
   onSubmit = (formValues) => {
     console.log(formValues);
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -54,7 +57,9 @@ const validate = ({ title, description }) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
